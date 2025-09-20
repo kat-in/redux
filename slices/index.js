aa566cf079b0ebdb86fea1cea3fc0180eeb53593
Kat <kat@MacBook-Pro-user.local>
2025-09-20 18:41:38 +0300

    подключили редьюсер к общему хранилищу

--- slices/index.js
+++ slices/index.js
@@ -1,24 +1,9 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './counterSlice.js'
 
-const initialState = {
-    value: 0,
-}
-
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState,
-    // Редьюсеры в слайсах меняют состояние и ничего не возвращают
-    reducers: {
-        increment: (state) => { state.value += 1 },
-        decrement: (state) => {state.value -= 1 }, 
-        incrementByAmount: (state, action) => {state.value += action.payload },
-    }
+export default configureStore({
+  reducer: {
+    // Свойство counter будет внутри объекта общего состояния: state.counter
+    counter: counterReducer,
+  },
 })
-
-// Слайс генерирует действия, которые экспортируются отдельно
-// Действия генерируются автоматически из имен ключей редьюсеров
-
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
-
-// По умолчанию экспортируется редьюсер, сгенерированный слайсом
-export default counterSlice.reducer
